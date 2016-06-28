ced57438c0424006e4801dfa98c2049dd824306e
WeiRenChen <vic4113110631@yahoo.com.tw>
2016-06-28 15:53:11 +0800

    PPT增加操作說明，Relation.do增加學號防堵機制。

--- src/com/web/RelationServlet.java
+++ src/com/web/RelationServlet.java
@@ -1,8 +1,10 @@
 package com.web;
 
 import com.model.*;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -12,6 +14,9 @@ import javax.servlet.http.*;
 public class RelationServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setCharacterEncoding("UTF-8"); // 先指定輸出的編碼
+		PrintWriter out = response.getWriter(); // 再拿到輸出對象
+		response.setContentType("text/html;charset=UTF-8");
 
 		RequestDispatcher view = null;
 		DataAnalysis dataAnalysis = new DataAnalysis();
@@ -21,7 +26,24 @@ public class RelationServlet extends HttpServlet {
 		ApplicantProcess applicantProcess = (ApplicantProcess)getServletContext().getAttribute("apply");
 		String kwd = (String)request.getParameter("kwd");
 
-		if(kwd != null){
+		/* Start 確認學號是否有效的 */
+		Boolean isEffectiveKwd = Boolean.FALSE;
+		ArrayList<Event> eventList = (ArrayList<Event>)getServletConfig().getServletContext().getAttribute("EventList");
+		for (Event event : eventList)
+		{
+			for(Applicant applicant : event.getApplicantList())
+			{
+				if(applicant.getNumber().equals(kwd))
+				{
+					isEffectiveKwd = Boolean.TRUE;
+					break;
+				}
+			}
+		}
+		System.out.println(isEffectiveKwd);
+		/* End 確認學號是否有效的 */
+
+		if(isEffectiveKwd){
 			ArrayList<Event> mainevents = applicantProcess.getYourEvents(eventProcess, kwd);
 			for (Event e : mainevents){
 				System.out.println(e.getApplicantList());
@@ -33,7 +55,10 @@ public class RelationServlet extends HttpServlet {
 			request.setAttribute("jsonStrg", jString);
 			request.setAttribute("relArray", result);
 			view = request.getRequestDispatcher("Relation.jsp");
-		}else view = request.getRequestDispatcher("index.jsp");
+		}else {
+			out.println("沒有此學號，5秒後轉跳頁面</br>");
+			response.addHeader("refresh", "2;URL=Index.do");
+		}
 
 		view.forward(request, response);
 	}
